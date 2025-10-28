1f0ef9f5682d6f2dd4e4b8c5efbb0a3fbdcfc35f
zoheir kabuli <zoheyrk@gmail.com>
2025-10-28 19:29:23 +0330

    Add CSS modules for Header, Hero, and Footer components

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>$ echo "Built by Zoheir Kabuli" &gt; Sydney, Australia 2025</p>
+    </footer>
+  );
+}
