1bd00a16d30a9c9d63d338fef599d015245a5d24
zoheir kabuli <zoheyrk@gmail.com>
2025-10-29 10:07:36 +0330

    fix: update footer text formatting
    - Change HTML entities in footer component for proper rendering of quotes and greater readability.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,9 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>$ echo "Built by Zoheir Kabuli" &gt; Sydney, Australia 2025</p>
+      <p>
+        $ echo &quot;Built by Zoheir Kabuli&quot; &gt; Sydney, Australia 2025
+      </p>
     </footer>
   );
 }
